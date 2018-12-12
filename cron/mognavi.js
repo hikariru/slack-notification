ce5af115efe6243904418458ff6b70f55889894b
hikariru <hkr.uchym@gmail.com>
2018-12-13 07:43:11 +0900

    元ネタをわかりやすく

--- cron/mognavi.js
+++ cron/mognavi.js
@@ -3,7 +3,7 @@ const CronJob = require('cron').CronJob;
 module.exports = bot => {
   new CronJob('0 0 9 * * 2', () => {
     bot.say({
-      text: `火曜日だぁああ！！ :ice_cream: \n https://mognavi.jp/do/whats_new/show :custard:`,
+      text: `やったぁあああ！火曜日だぁああ！！:ice_cream::custard:\n https://mognavi.jp/do/whats_new/show`,
       channel: process.env.CHANNEL_ID,
     });
   }, null, true, process.env.TIME_ZONE);
