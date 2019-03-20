a6a37ebde4f3acf1075caedd392fbf6d544ab038
hikariru <hkr.uchym@gmail.com>
2019-03-21 04:00:44 +0900

    did some refactoring

--- cron/garbage.js
+++ cron/garbage.js
@@ -4,7 +4,10 @@ const TimeDate = require('time').Date;
 /** @type Array */
 const garbageList = require('../constants/garbage.json');
 
-const garbageType = () => {
+/**
+ * @return {string}
+ */
+const resolveGarbageType = () => {
   const date = new TimeDate();
   date.setTimezone(process.env.TIME_ZONE);
   const day = date.getDay();
@@ -15,7 +18,7 @@ const garbageType = () => {
 module.exports = bot => {
   new CronJob('0 0 7 * * 1-5', () => {
     bot.say({
-      text: `おはようございます :sun_with_face: 今日は *${garbageType()}* の収集日です :wastebasket:`,
+      text: `おはようございます :sun_with_face: 今日は *${resolveGarbageType()}* の収集日です :wastebasket:`,
       channel: process.env.CHANNEL_ID,
     });
   }, null, true, process.env.TIME_ZONE);
--- cron/mognavi.js
+++ cron/mognavi.js
@@ -1,9 +1,11 @@
 const CronJob = require('cron').CronJob;
 
+const mogNaviURL = 'https://mognavi.jp/do/whats_new/show';
+
 module.exports = bot => {
   new CronJob('0 0 9 * * 2', () => {
     bot.say({
-      text: `やったぁあああ！火曜日だぁああ！！:ice_cream::custard:\n https://mognavi.jp/do/whats_new/show`,
+      text: `やったぁあああ！火曜日だぁああ！！:ice_cream::custard:\n ${mogNaviURL}`,
       channel: process.env.CHANNEL_ID,
     });
   }, null, true, process.env.TIME_ZONE);
