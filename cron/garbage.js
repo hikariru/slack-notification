89168941ac2476ee5180a09bf5027ea61882c7ea
Hikaru Uchiyama <hikariru@users.noreply.github.com>
2019-04-01 17:38:54 +0900

    Update garbage.js

--- cron/garbage.js
+++ cron/garbage.js
@@ -4,7 +4,7 @@ module.exports = bot => {
   // 火・金
   new CronJob('0 0 7 * * 2,5', () => {
     bot.say({
-      text: `おはようございます :sun_with_face: 今日は 燃やすごみ の収集日です :wastebasket:`,
+      text: "おはようございます :sun_with_face: 今日は 燃やすごみ の収集日です :wastebasket:",
       channel: process.env.CHANNEL_ID,
     });
   }, null, true, process.env.TIME_ZONE);
@@ -12,8 +12,8 @@ module.exports = bot => {
   // 月・水・木
   new CronJob('0 0 7 * * 1,3,4', () => {
     bot.say({
-      text: `おはようございます :sun_with_face: 収集カレンダーをチェックしましょう :wastebasket:
-      ${process.env.GC_CALENDAR_PDF_URL}`,
+      text: "おはようございます :sun_with_face: 収集カレンダーをチェックしましょう :wastebasket:\n" +
+        process.env.GC_CALENDAR_PDF_URL,
       channel: process.env.CHANNEL_ID,
     });
   }, null, true, process.env.TIME_ZONE);
