db91595c37e299fda7cd22ae3b7e8d049af8c1a9
hikariru <hkr.uchym@gmail.com>
2019-07-17 22:39:47 +0900

    様子見

--- bootstrap.js
+++ bootstrap.js
@@ -7,9 +7,9 @@ const {
   , SlackBotWorker
   , SlackEventMiddleware
 } = require('botbuilder-adapter-slack');
-const Restify = require('restify');
-const Fs = require('fs');
 const Path = require('path');
+const Fs = require('fs');
+
 
 const adapter = new SlackAdapter({
   clientSigningSecret: process.env.SLACK_SIGNING_SECRET
@@ -27,7 +27,9 @@ const controller = new Botkit({
 
 controller.ready(() => {
   controller.loadModules(__dirname + '/events');
-  // controller.loadModules(__dirname + '/jobs');
+
+  const bot = new SlackBotWorker(controller, {});
+  loadFunctions('jobs', bot);
 });
 
 controller.webserver.get('/', (req, res) => {
@@ -36,42 +38,23 @@ controller.webserver.get('/', (req, res) => {
 });
 
 
-// /** @type {SlackBotWorker} */
-// const bot = new SlackBotWorker(controller, {});
+/**
+ * @param {string} directoryName
+ * @param {Object} attribute
+ */
+const loadFunctions = (directoryName, attribute) => {
+  const directoryPath = Path.resolve('.', directoryName);
 
-
-// /**
-//  * @param {string} directoryName
-//  * @param {Object} module
-//  */
-// const loadDirectory = (directoryName, module) => {
-//   const directoryPath = Path.resolve('.', directoryName);
-//
-//   Fs.readdirSync(directoryPath).forEach((file) => {
-//     const extension = Path.extname(file);
-//     const fullPath = Path.join(directoryPath, Path.basename(file, extension));
-//     try {
-//       const script = require(fullPath);
-//       if (typeof script === 'function') {
-//         script(module, adapter);
-//       }
-//     } catch(error) {
-//       process.exit(1);
-//     }
-//   });
-// };
-//
-// loadDirectory('jobs', bot);
-// loadDirectory('events', controller);
-//
-// /** @type {Server} */
-// const server = Restify.createServer();
-// server.listen();
-// server.use(Restify.plugins.acceptParser(server.acceptable));
-// server.use(Restify.plugins.queryParser());
-// server.use(Restify.plugins.bodyParser());
-//
-// server.get('/', function (req, res, next) {
-//   res.send(200, 'hi :)');
-//   next();
-// });
+  Fs.readdirSync(directoryPath).forEach((file) => {
+    const extension = Path.extname(file);
+    const fullPath = Path.join(directoryPath, Path.basename(file, extension));
+    try {
+      const script = require(fullPath);
+      if (typeof script === 'function') {
+        script(attribute);
+      }
+    } catch (error) {
+      process.exit(1);
+    }
+  });
+};
