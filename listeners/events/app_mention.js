9698bf174427e9f341e2cc54ebed467c18dbce37
hikariru <hkr.uchym@gmail.com>
2020-05-12 18:56:11 +0900

    okay.

--- listeners/events/app_mention.js
+++ listeners/events/app_mention.js
@@ -1,10 +1,9 @@
 module.exports = app => {
   app.event('app_mention', async ({ event, context }) => {
-    console.log(event);
     await app.client.chat.postMessage({
         token: context.botToken,
         channel: event.channel,
-        text: `<@${event.user.id}> はーい`
+        text: `<@${event.user}> はーい`
       });
   });
 };
