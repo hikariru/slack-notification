05d4d114c8f1a1afff64f3b8b7a1784b021ef121
hikariru <hkr.uchym@gmail.com>
2020-05-12 18:51:22 +0900

    I accept my ignorance of async/await

--- listeners/events/app_mention.js
+++ listeners/events/app_mention.js
@@ -1,5 +1,6 @@
 module.exports = app => {
   app.event('app_mention', async ({ event, context }) => {
+    console.log(event);
     await app.client.chat.postMessage({
         token: context.botToken,
         channel: event.channel,
