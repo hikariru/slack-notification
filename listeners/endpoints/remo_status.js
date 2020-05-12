ace7c505ddd11f606cfe898aa36df1521a656bd1
hikariru <hkr.uchym@gmail.com>
2020-05-12 18:49:30 +0900

    I accept my ignorance of async/await

--- listeners/endpoints/remo_status.js
+++ listeners/endpoints/remo_status.js
@@ -3,12 +3,16 @@ const getRemoStatus = require('../../modules/get_remo_status');
 module.exports = app => {
   app.receiver.app.get(`/slack/remo_status`, async(req, res) => {
     res.sendStatus(200);
-
-    const remoStatus = await getRemoStatus();
-    return await app.client.chat.postMessage({
-      token: process.env.SLACK_BOT_TOKEN,
-      channel: process.env.CHANNEL_ID,
-      text: `:thermometer: ${remoStatus.value}℃ (${remoStatus.createdAt})`,
-    });
+    try {
+      const remoStatus = await getRemoStatus();
+      console.log(remoStatus);
+      return app.client.chat.postMessage({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: process.env.CHANNEL_ID,
+        text: `:thermometer: ${remoStatus.value}℃ (${remoStatus.createdAt})`,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   });
 };
