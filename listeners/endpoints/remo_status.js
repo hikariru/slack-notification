68ef6a05ebdfd1666390b49bcd0c1ea9d18f116b
hikariru <hkr.uchym@gmail.com>
2020-05-12 23:08:37 +0900

    うるさい

--- listeners/endpoints/remo_status.js
+++ listeners/endpoints/remo_status.js
@@ -1,8 +1,17 @@
+const moment = require('moment-timezone');
 const getRemoStatus = require('../../modules/get_remo_status');
 
 module.exports = app => {
   app.receiver.app.get(`/slack/remo_status`, async(req, res) => {
     res.sendStatus(200);
+
+    const currentHour = moment().timeZone(process.env.TIME_ZONE).getHours();
+
+    // うるさいので3時間ごとにしかPOSTさせない
+    if (currentHour % 3 !== 0) {
+      return;
+    }
+
     try {
       const remoStatus = await getRemoStatus();
       return app.client.chat.postMessage({
