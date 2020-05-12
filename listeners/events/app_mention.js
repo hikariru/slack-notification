98fc94cb6e86b0fb81233b2287ba2e3a00de138b
hikariru <hkr.uchym@gmail.com>
2020-05-12 18:40:33 +0900

    totally my bad

--- listeners/events/app_mention.js
+++ listeners/events/app_mention.js
@@ -1,15 +1,9 @@
 module.exports = app => {
   app.event('app_mention', async ({ event, context }) => {
-    try {
-      const result = await app.client.chat.postMessage({
+    await app.client.chat.postMessage({
         token: context.botToken,
-        channel: context.channel,
+        channel: event.channel,
         text: `<@${event.user.id}> はーい`
       });
-      console.log(result);
-    }
-    catch (error) {
-      console.error(error);
-    }
   });
 };
