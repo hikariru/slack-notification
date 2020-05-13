f189712b00c1eee526fd5c9b8f3c0282b3518fe5
hikariru <hkr.uchym@gmail.com>
2020-05-13 13:17:09 +0900

    いきなり静かになるな

--- listeners/endpoints/remo_status.js
+++ listeners/endpoints/remo_status.js
@@ -5,7 +5,7 @@ module.exports = app => {
   app.receiver.app.get(`/slack/remo_status`, async(req, res) => {
     res.sendStatus(200);
 
-    const currentHour = moment().tz(process.env.TIME_ZONE).getHours();
+    const currentHour = Number(moment().tz(process.env.TIME_ZONE).hour());
 
     // うるさいので3時間ごとにしかPOSTさせない
     if (currentHour % 3 !== 0) {
