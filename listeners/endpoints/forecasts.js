e58310875448ec9172168e7e394ef1f1a137051b
hikariru <hkr.uchym@gmail.com>
2020-05-14 17:40:05 +0900

    プリンセス天気(わらうところ)

--- listeners/endpoints/forecasts.js
+++ listeners/endpoints/forecasts.js
@@ -1,10 +1,10 @@
 const moment = require('moment-timezone');
 
 const getWeatherReports = require('../../modules/get_weather_reports');
-
+const buildForecastMessages = require('../../modules/build_forecast_messages');
 
 module.exports = app => {
-  app.receiver.app.get(`/weather`, async (req, res) => {
+  app.receiver.app.get(`/slack/weather`, async (req, res) => {
     res.status(200).send('OK');
 
     // const currentHour = Number(moment().tz(process.env.TIMEZONE).hour());
