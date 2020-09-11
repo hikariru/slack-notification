32393e9b20de970fb0fd97adf6a34759512f2fa3
hikaru <hikariru@users.noreply.github.com>
2020-09-11 22:42:29 +0900

    adding mention

--- listeners/endpoints/remo_status.js
+++ listeners/endpoints/remo_status.js
@@ -1,5 +1,7 @@
 const moment = require('moment-timezone');
 const getRemoStatus = require('../../modules/get_remo_status');
+const maxTemperature = 28;
+const minTemperature = 17;
 
 module.exports = app => {
   app.receiver.app.get(`/slack/remo_status`, async(req, res) => {
@@ -14,11 +16,17 @@ module.exports = app => {
     try {
       const remoStatus = await getRemoStatus();
       const temperature = Math.round(remoStatus.value);
+      let text = `室温は ${temperature}℃です :thermometer: (${remoStatus.createdAt})`;
+
+      // 事務所衛生基準規則5条3項
+      if (temperature > maxTemperature || temperature < minTemperature) {
+        text = '<!channel> ' + text;
+      }
 
       return app.client.chat.postMessage({
         token: process.env.SLACK_BOT_TOKEN,
         channel: process.env.WEATHER_CHANNEL_ID,
-        text: `室温は ${temperature}℃です :thermometer: (${remoStatus.createdAt})`,
+        text: text,
       });
     } catch (err) {
       console.error(err);
