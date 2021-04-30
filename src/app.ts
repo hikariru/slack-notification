61209d6a9460ce5e464d07b14f9938993172905b
Hikaru Uchiyama <hkr.uchym@gmail.com>
2021-04-30 14:53:02 +0900

    謎の多重ロードが起きてる

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,6 @@ fs.readdirSync(listenersRoot).forEach((directory :string) => {
   const directoryRoot = path.join(listenersRoot, directory);
   fs.readdirSync(directoryRoot).forEach((file :string) => {
     const extension = path.extname(file);
-    console.log(extension);
     if (extension !== ".js") {
       return;
     }
