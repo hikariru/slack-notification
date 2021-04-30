5dcda2e401e327e9405127642b3b5290cfe0e7df
Hikaru Uchiyama <hkr.uchym@gmail.com>
2021-04-30 14:46:54 +0900

    にゃーん

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ fs.readdirSync(listenersRoot).forEach((directory :string) => {
   const directoryRoot = path.join(listenersRoot, directory);
   fs.readdirSync(directoryRoot).forEach((file :string) => {
     const extension = path.extname(file);
-    if (extension === "") {
+    if (extension !== ".js") {
       return;
     }
     const fullPath = path.join(directoryRoot, path.basename(file, extension));
