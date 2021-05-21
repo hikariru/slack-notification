b1c2c09af29a6da96da29cd7e03d18b33cd2f8e9
Hikaru Uchiyama <hkr.uchym@gmail.com>
2021-05-21 09:35:58 +0900

    アーーー

--- src/listeners/commands/ping.ts
+++ src/listeners/commands/ping.ts
@@ -3,7 +3,7 @@ import {App, ExpressReceiver} from '@slack/bolt';
 module.exports = (app: App, receiver: ExpressReceiver) => {
   app.command('ping', async ({command, ack, say}) => {
     await ack();
-    await say(`<@${command.user_id}> pong!'`);
+    await say(`pong!'`);
     return;
   })
 };
